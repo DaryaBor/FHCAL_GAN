02edcbf9d397711a19c3da9cc82d321645eef518
DaryaBor <110776866+DaryaBor@users.noreply.github.com>
2026-09-22 23:23:14 +0300

    Reduce training epochs from 30 to 15

--- GAN_project/config/run_experiment.py
+++ GAN_project/config/run_experiment.py
@@ -153,7 +153,7 @@ def form_gan_trainer(model_name: str, gan_model: Optional[GAN] = None, n_epochs:
 
 def run() -> GAN:
     model_name = 'physics_test_enerqy_aware'
-    gan_trainer, epoch_trainer = form_gan_trainer(model_name=model_name, n_epochs=30)
+    gan_trainer, epoch_trainer = form_gan_trainer(model_name=model_name, n_epochs=15)
     gan = None
     for epoch, gan in gan_trainer:
         pass
